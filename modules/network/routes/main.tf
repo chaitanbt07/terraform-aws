7f56838ca365e8833ed96bd7c9a2236ed003fcca
Raghavan R <raghavan.r@capgemini.com>
2018-08-09 09:12:28 +1000

    changing the values in igw to accept null values

--- modules/network/routes/main.tf
+++ modules/network/routes/main.tf
@@ -1,6 +1,6 @@
 resource "aws_route" "route" {
     route_table_id = "${var.route_table_id}"
     destination_cidr_block = "${var.destination_cidr_block}"
-    gateway_id = "${var.gateway_id != "0" ? var.gateway_id : 0}" # Either IGW or VPW
-    nat_gateway_id = "${var.nat_gateway_id != "0" ? var.nat_gateway_id : 0}"
+    gateway_id = "${var.gateway_id != "false" ? var.gateway_id : 0}" # Either IGW or VPW
+    nat_gateway_id = "${var.nat_gateway_id != "false" ? var.nat_gateway_id : 0}"
 }
--- modules/network/routes/variable.tf
+++ modules/network/routes/variable.tf
@@ -1,4 +1,8 @@
 variable "route_table_id" {}
 variable "destination_cidr_block" {}
-variable "nat_gateway_id" {}
-variable "gateway_id" {}
+variable "nat_gateway_id" {
+    default = "false"
+}
+variable "gateway_id" {
+    default = "false"
+}
