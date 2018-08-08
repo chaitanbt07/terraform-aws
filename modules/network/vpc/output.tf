68eee8c242e0d0d7e5318833bf5a6a8e1f1f17af
Raghavan R <raghavan.r@capgemini.com>
2018-08-08 16:39:21 +1000

    restoring original value

--- modules/network/vpc/output.tf
+++ modules/network/vpc/output.tf
@@ -1,27 +1,27 @@
 output "id" {
-    value = "${element(concat(aws_vpc.core-network.*.id, list("")), 1)}"
+    value = "${element(concat(aws_vpc.core-network.*.id, list("")), 0)}"
 }
 
 output "vpcarn" {
-    value = "${element(concat(aws_vpc.core-network.*.arn, list("")), 1)}"
+    value = "${element(concat(aws_vpc.core-network.*.arn, list("")), 0)}"
 }
 
 output "vpc-cidr" {
-    value = "${element(concat(aws_vpc.core-network.*.cidr_block, list("")), 1)}"
+    value = "${element(concat(aws_vpc.core-network.*.cidr_block, list("")), 0)}"
 }
 
 output "main-rt" {
-    value = "${element(concat(aws_vpc.core-network.*.main_route_table_id, list("")), 1)}"
+    value = "${element(concat(aws_vpc.core-network.*.main_route_table_id, list("")), 0)}"
 }
 
 output "default-nacl" {
-    value = "${element(concat(aws_vpc.core-network.*.default_network_acl_id, list("")), 1)}"
+    value = "${element(concat(aws_vpc.core-network.*.default_network_acl_id, list("")), 0)}"
 }
 
 output "default-sg" {
-    value = "${element(concat(aws_vpc.core-network.*.default_security_group_id, list("")), 1)}"
+    value = "${element(concat(aws_vpc.core-network.*.default_security_group_id, list("")), 0)}"
 }
 
 output "default-rt" {
-    value = "${element(concat(aws_vpc.core-network.*.default_route_table_id, list("")), 1)}"
+    value = "${element(concat(aws_vpc.core-network.*.default_route_table_id, list("")), 0)}"
 }
