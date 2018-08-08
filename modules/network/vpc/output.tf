ed57660465f7dea40b2ceb9b1b8ccd7a8b5215b2
Raghavan R <raghavan.r@capgemini.com>
2018-08-08 16:15:44 +1000

    fixing the type error.

--- modules/network/vpc/output.tf
+++ modules/network/vpc/output.tf
@@ -1,27 +1,27 @@
 output "id" {
-    value = "${element(concat(aws_vpc.core-network.*.id, list("")), count.index)}"
+    value = "${element(concat(aws_vpc.core-network.*.id, list("")), 1)}"
 }
 
 output "vpcarn" {
-    value = "${element(concat(aws_vpc.core-network.*.arn, list("")), count.index)}"
+    value = "${element(concat(aws_vpc.core-network.*.arn, list("")), 1)}"
 }
 
 output "vpc-cidr" {
-    value = "${element(concat(aws_vpc.core-network.*.cidr_block, list("")), count.index)}"
+    value = "${element(concat(aws_vpc.core-network.*.cidr_block, list("")), 1)}"
 }
 
 output "main-rt" {
-    value = "${element(concat(aws_vpc.core-network.*.main_route_table_id, list("")), count.index)}"
+    value = "${element(concat(aws_vpc.core-network.*.main_route_table_id, list("")), 1)}"
 }
 
 output "default-nacl" {
-    value = "${element(concat(aws_vpc.core-network.*.default_network_acl_id, list("")), count.index)}"
+    value = "${element(concat(aws_vpc.core-network.*.default_network_acl_id, list("")), 1)}"
 }
 
 output "default-sg" {
-    value = "${element(concat(aws_vpc.core-network.*.default_security_group_id, list("")), count.index)}"
+    value = "${element(concat(aws_vpc.core-network.*.default_security_group_id, list("")), 1)}"
 }
 
 output "default-rt" {
-    value = "${element(concat(aws_vpc.core-network.*.default_route_table_id, list("")), count.index)}"
+    value = "${element(concat(aws_vpc.core-network.*.default_route_table_id, list("")), 1)}"
 }
