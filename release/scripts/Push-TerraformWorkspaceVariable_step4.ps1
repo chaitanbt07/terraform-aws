f08ae61a573377c86ba0246f4c9878f64a59e50a
Raghavan R <33410658+raghavanr@users.noreply.github.com>
2018-09-04 15:42:16 +1000

    Update Push-TerraformWorkspaceVariable_step4.ps1

--- release/scripts/Push-TerraformWorkspaceVariable_step4.ps1
+++ release/scripts/Push-TerraformWorkspaceVariable_step4.ps1
@@ -52,8 +52,6 @@
             {
                 #$Credential | % {if (($_.key -match "secret") -or ($_.key -match "access")) { $sensitive,$hcl,$keyname = $true,$false,$Credential.key}else{$sensitive,$hcl,$keyname =  $false,$true,$($_.key.SubString($_.key.LastIndexOf('_')+1))}}
 		$Credential | % {if (($_.key -match "secret") -or ($_.key -match "access")) { $sensitive,$hcl,$keyname = $true,$false,$Credential.key}else{$sensitive,$hcl,$keyname =  $false,$false,$($_.key.remove(0,4))}}
-		Write-host "Variable $Credential.key State: $sensitive"
-		Write-Host "Variable $Credential.key Type: $hcl"
 		$Json = @{
                   "data"= @{
                     "type"="vars"
