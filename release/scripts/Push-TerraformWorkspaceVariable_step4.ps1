bd9fcbdd42b2586d7eb2f6ce239e78a7edd0c062
Raghavan R <33410658+raghavanr@users.noreply.github.com>
2018-09-04 15:38:42 +1000

    Update Push-TerraformWorkspaceVariable_step4.ps1

--- release/scripts/Push-TerraformWorkspaceVariable_step4.ps1
+++ release/scripts/Push-TerraformWorkspaceVariable_step4.ps1
@@ -59,7 +59,7 @@
                     "type"="vars"
                     "attributes"= @{
                       "key"=$keyname
-                      "value"=$Credential.value
+                      "value"="$Credential.value"
                       "category"="terraform"
                       "hcl"=$hcl
                       "sensitive"=$sensitive
