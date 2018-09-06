9c8f2ee786da71145c99964001a2bd8ce5d2fbe1
Raghavan R <raghavan.r@capgemini.com>
2018-09-06 15:02:09 +1000

    adding sentinel policy api

---
+++ release/scripts/New-SentinalPolicy.ps1
@@ -0,0 +1,76 @@
+[CmdletBinding()]
+[Alias()]
+[OutputType([int])]
+Param
+(
+
+    [Parameter(Mandatory = $true,
+        ValueFromPipelineByPropertyName = $true,
+        Position = 0)]
+    $OrganizationName,
+
+    #Token
+    [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true, 
+        Position = 3)]
+    $Token
+
+)
+
+Begin {
+
+    Write-Host "$($MyInvocation.MyCommand.Name): Script execution started"
+    
+    $Json = @{
+        "data" = @{
+            
+            "attributes" = @{
+                "enforce" = @{
+                    [
+                    @{
+                    "path" = "policy1.sentinel",
+                    "mode" = "hard-mandatory"
+                    }
+                    ],
+                },
+                "name": "policy1",
+            },
+            "type"="policies"
+        }
+    }
+
+    $Json
+
+    $Post = @{
+        Uri = "https://app.terraform.io/api/v2/organizations/$OrganizationName/policies"
+        Headers     = @{"Authorization" = "Bearer $Token" }
+        ContentType = 'application/vnd.api+json'
+        Method      = 'Post'
+        Body        = $Json
+        ErrorAction = 'stop'
+    }
+}
+
+Process {
+    try {
+        $Result = (Invoke-RestMethod @Post).data
+        Write-Output "TFE_POLICY_ID=$($Result.id)" |out-file ./TFE_POLICYID.txt
+        Get-ChildItem
+        Return $Result
+    }
+    catch {
+        $ErrorID = ($Error[0].ErrorDetails.Message | ConvertFrom-Json).errors.status
+        $Message = ($Error[0].ErrorDetails.Message | ConvertFrom-Json).errors.detail
+        $Exception = ($Error[0].ErrorDetails.Message | ConvertFrom-Json).errors.title
+
+        Write-Error -Exception $Exception -Message $Message -ErrorId $ErrorID
+    }
+    finally {
+        If ($Result) {
+            Write-Host "$($MyInvocation.MyCommand.Name): Script execution complete"
+        }
+    }
+}
+End {
+
+    Write-Host "$($MyInvocation.MyCommand.Name): Script execution complete."
+}
