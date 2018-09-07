e4ff227a6a155bb4fffb187007714c76a292ae10
Raghavan R <raghavan.r@capgemini.com>
2018-09-07 16:02:30 +1000

    getting contents

--- release/scripts/New-SentinalPolicy.ps1
+++ release/scripts/New-SentinalPolicy.ps1
@@ -67,7 +67,7 @@ Process {
             Get-ChildItem
             Write-Host $Result
 
-            cat ./$Policy
+            Get-Content ./$Policy
 
             #$Put = @{
              #   Uri = "https://app.terraform.io/api/v2/policies/$($Result.id)/upload"
