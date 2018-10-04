00c18a8edfc4b2cb8e9cc3ea1a371681335fbc8d
Raghavan R <33410658+raghavanr@users.noreply.github.com>
2018-10-04 15:50:41 +0530

    Update pythontest.py

--- release/scripts/pythontest.py
+++ release/scripts/pythontest.py
@@ -1,7 +1,8 @@
 import os
+import subprocess
 myenv = os.getenv('aws_access_key', default="Sample")
 print(myenv)
 #print(str(os.environ['aws']))
-a = subprocess.Popen("env | grep 'AWS*'", shell=True, stdout=subprocess.PIPE).stdout
+a = subprocess.Popen("env | grep -i 'AWS*'", shell=True, stdout=subprocess.PIPE).stdout
 b = a.read()
 b.decode("utf-8")
