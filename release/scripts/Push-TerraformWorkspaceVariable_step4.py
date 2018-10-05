7916cf8b29662adfa11e902770a6b419ca9c68aa
Raghavan R <raghavan.r@capgemini.com>
2018-10-05 16:48:06 +0530

    Auto stash before rebase of "origin/master"

--- release/scripts/Push-TerraformWorkspaceVariable_step4.py
+++ release/scripts/Push-TerraformWorkspaceVariable_step4.py
@@ -11,7 +11,24 @@ def workspacevariable(WorkSpaceID="", Provider="", Token=""):
         os.system("clear")
     print("\n##########################################################################################\n")
     print("Script Execution Started")
+    try:
+        a = subprocess.Popen("env | grep 'bamboo_" + Provider + "_*'", shell=True, stdout=subprocess.PIPE).stdout
+        b = a.read()
+        b = b.decode("utf-8")
+        b = b.split("\n")
+        b.pop()
+        c =[]
+        env_vars ={}
+        for i in b:
+            c.append(i.split("="))
+        for i in c:
+            
+            env_vars[i[0]] = i[1]
+        print(env_vars)
+    except Exception as e:
+        print("\033[1;31mError : " + str(e) + "\033[0m")
     #a = subprocess.Popen("env | grep 'bamboo_" + Provider + "_*'", shell=True, stdout=subprocess.PIPE).stdout
+<<<<<<< Updated upstream
     a = subprocess.Popen("env | grep 'bamboo_" + Provider + "_*'", shell=True, stdout=subprocess.PIPE).stdout
     b = a.read()
     b = b.decode("utf-8")
@@ -26,6 +43,11 @@ def workspacevariable(WorkSpaceID="", Provider="", Token=""):
     #print(env_vars[0])
 
     #print(env_vars)
+=======
+    finally:
+        print("Script Execution Completed")
+        print("\n##########################################################################################\n")
+>>>>>>> Stashed changes
 
 def main():
     workspaceid = sys.argv[1]
