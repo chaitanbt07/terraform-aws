0f96738a2c01d1d0aee7c8cf802b76feb23387d4
Raghavan R <33410658+raghavanr@users.noreply.github.com>
2018-10-05 12:27:39 +0530

    Update pythontest.py

--- release/scripts/pythontest.py
+++ release/scripts/pythontest.py
@@ -4,7 +4,7 @@ import sys
 myenv = os.getenv('aws_access_key', default="Sample")
 print(myenv)
 #print(str(os.environ['aws']))
-a = subprocess.Popen("env | grep '" + sys.argv[1] + "_*'", shell=True, stdout=subprocess.PIPE).stdout
+a = subprocess.Popen("env | grep 'bamboo_" + sys.argv[1] + "_*'", shell=True, stdout=subprocess.PIPE).stdout
 b = a.read()
 b = b.decode("utf-8")
 b = b.split("\n")
