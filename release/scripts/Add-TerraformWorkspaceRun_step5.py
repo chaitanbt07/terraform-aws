6f23910af1483c63a58d1bd0c7c32a70bd551f71
Raghavan R <raghavan.r@capgemini.com>
2018-10-10 17:26:22 +0530

    adding comment for run

--- release/scripts/Add-TerraformWorkspaceRun_step5.py
+++ release/scripts/Add-TerraformWorkspaceRun_step5.py
@@ -16,9 +16,8 @@ def workspacerun():
         buildkey = subprocess.Popen("env | grep 'bamboo_buildKey'", shell=True, stdout=subprocess.PIPE).stdout
         buildnum = subprocess.Popen("env | grep 'bamboo_buildResultKey'", shell=True, stdout=subprocess.PIPE).stdout
         buildkey = ((buildkey.read()).decode("utf-8")).split("=")
-        print(buildkey)
-
-        comment = "Run Requested by Release for " + buildkey + " build number "
+        buildnum = ((buildnum.read()).decode("utf-8")).split("=")
+        comment = "Run Requested by Release for " + buildkey[1].replace("\n", "") + " build number " + buildnum[1].replace("\n", "")
         print(comment)
     except Exception as e:
         print("Error: " + str(e) + "\n")
