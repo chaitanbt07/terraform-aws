6cbd926c683845c70cadd95c2d36f62dbe4cbdc7
Raghavan R <raghavan.r@capgemini.com>
2018-10-10 16:21:21 +0530

    type

--- release/scripts/Add-TerraformWorkspaceRun_step5.py
+++ release/scripts/Add-TerraformWorkspaceRun_step5.py
@@ -15,7 +15,7 @@ def workspacerun():
         f = open("TFE_RUNID.txt", "a+")
         buildkey = subprocess.Popen("env | grep 'bamboo_buildKey'", shell=True, stdout=subprocess.PIPE).stdout
         buildnum = subprocess.Popen("env | grep 'bamboo_buildResultKey'", shell=True, stdout=subprocess.PIPE).stdout
-        comment = "Run Requested by Release " +   + "for " + buildkey + " build number " + buildnum
+        comment = "Run Requested by Release for " + buildkey + " build number " + buildnum
         print(comment)
     except Exception as e:
         print("Error: " + str(e) + "\n")
