8713aa8ce1f2c179d6453b177af3710941270817
Raghavan R <raghavan.r@capgemini.com>
2018-10-10 10:32:43 +0530

    removing junks

--- release/scripts/Push-TerraformWorkspaceVariable_step4.py
+++ release/scripts/Push-TerraformWorkspaceVariable_step4.py
@@ -35,12 +35,11 @@ def workspacevariable(WorkSpaceID, Provider, Token, OrganizationName, WorkspaceN
                 headers = json.loads(headers_content)
                 url = "https://app.terraform.io/api/v2/vars"
                 result = requests.post(url, json = payload, headers = headers, allow_redirects = False)
-                print(result.content)
+                print((result.content)['detail'])
                 if result.status_code in range(200, 202):
                     f.write(key + "=" + (json.loads(result.content))['data']['id'] + "\n")
                     print("\033[1;32mVariable " + key + " Successfully uploaded to Workspace...\033[0m")
                 elif result.status_code == 422:
-                    print(result.__attrs__)
                     url = "https://app.terraform.io/api/v2/vars?filter%5Borganization%5D%5Bname%5D=" + OrganizationName + "&filter%5Bworkspace%5D%5Bname%5D=" + WorkspaceName
                     headers_content ='{"Authorization" : "Bearer  ' + Token + '", "Content-Type" : "application/vnd.api+json", "charset" : "utf-8" }'
                     headers = json.loads(headers_content)
