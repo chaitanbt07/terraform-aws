c6c4fee49aef7d66d93b70835c927f9c371bfd05
Raghavan R <raghavan.r@capgemini.com>
2018-10-10 17:56:08 +0530

    debug

--- release/scripts/Add-TerraformWorkspaceRun_step5.py
+++ release/scripts/Add-TerraformWorkspaceRun_step5.py
@@ -3,8 +3,8 @@ import os
 import sys
 import subprocess
 
-#def workspacerun(WorkSpaceID,ConfigVersionID,Token):
-def workspacerun():
+def workspacerun(WorkSpaceID,ConfigVersionID):
+#def workspacerun():
     if os.name == 'nt':
         os.system("cls")
     else:
@@ -18,10 +18,16 @@ def workspacerun():
         buildkey = ((buildkey.read()).decode("utf-8")).split("=")
         buildnum = ((buildnum.read()).decode("utf-8")).split("=")
         comment = "Run Requested by Release for " + buildkey[1].replace("\n", "") + " build number " + buildnum[1].replace("\n", "")
-        print(comment)
+        print("\033[1;32m")
+        #payload = dict(
+         #   data=dict(attributes=dict(key=key, value=env_vars[key], category="terraform", hcl=False, sensitive=True),
+          #            relationships=dict(workspace=dict(data=dict(id=WorkSpaceID, type="workspaces")))), type="vars")
+        payload = dict(data=dict(attributes=dict(is-destroy=False, message=comment)), type="runs", relationships=dict(workspace=dict(data=dict(type="workspaces", id=WorkSpaceID)), configuration-version=dict(data=dict(type="configuration-versions", id=ConfigVersionID))))
     except Exception as e:
         print("Error: " + str(e) + "\n")
 
 def main():
+    workspaceid = sys.argv[1]
+    configid = sys.argv[2]
     workspacerun()
 main()
