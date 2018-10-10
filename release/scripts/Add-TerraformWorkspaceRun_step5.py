07226151c36bbec84186e84620f711dc717a1d13
Raghavan R <raghavan.r@capgemini.com>
2018-10-10 16:16:28 +0530

    adding run

---
+++ release/scripts/Add-TerraformWorkspaceRun_step5.py
@@ -0,0 +1,25 @@
+import requests
+import os
+import sys
+import subprocess
+
+#def workspacerun(WorkSpaceID,ConfigVersionID,Token):
+def workspacerun():
+    if os.name == 'nt':
+        os.system("cls")
+    else:
+        os.system("clear")
+    print("\n##########################################################################################\n")
+    print("Script Execution Started\n")
+    try:
+        f = open("TFE_RUNID.txt", "a+")
+        buildkey = subprocess.Popen("env | grep 'bamboo_buildKey'", shell=True, stdout=subprocess.PIPE).stdout
+        buildnum = subprocess.Popen("env | grep 'bamboo_buildResultKey'", shell=True, stdout=subprocess.PIPE).stdout
+        comment = "Run Requested by Release " +   + "for " + buildkey + " build number " + buildnum
+        print(comment)
+    except Exception as e:
+        print("Error: " + str(e) + "\n")
+
+def main():
+    workspacerun()
+main()
