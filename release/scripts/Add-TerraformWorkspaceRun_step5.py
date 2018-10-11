e8b5159917d99105f356e120cd92c0689639c14e
Raghavan R <raghavan.r@capgemini.com>
2018-10-11 09:21:27 +0530

    creating run

--- release/scripts/Add-TerraformWorkspaceRun_step5.py
+++ release/scripts/Add-TerraformWorkspaceRun_step5.py
@@ -2,32 +2,71 @@ import requests
 import os
 import sys
 import subprocess
+import json
 
-def workspacerun(WorkSpaceID,ConfigVersionID):
-#def workspacerun():
+def workspacerun(WorkSpaceID,ConfigVersionID, Token):
     if os.name == 'nt':
         os.system("cls")
     else:
         os.system("clear")
     print("\n##########################################################################################\n")
     print("Script Execution Started\n")
+    buildkey = subprocess.Popen("env | grep 'bamboo_buildKey'", shell=True, stdout=subprocess.PIPE).stdout
+    buildnum = subprocess.Popen("env | grep 'bamboo_buildResultKey'", shell=True, stdout=subprocess.PIPE).stdout
+    buildkey = ((buildkey.read()).decode("utf-8")).split("=")
+    buildnum = ((buildnum.read()).decode("utf-8")).split("=")
+    comment = "Run Requested by Release for " + buildkey[1].replace("\n", "") + " build number " + \
+              buildnum[1].replace("\n", "")
+    print("\033[1;32m")
+    payload = '''{
+                "data": {
+                    "attributes": {
+                        "is-destroy": false,
+                        "message": "''' + comment + '''"
+                        },
+                    "type": "runs",
+                    "relationships": {
+                        "workspace": {
+                            "data": {
+                                "type": "workspaces"
+                                "id": "''' + WorkSpaceID + '''"
+                            }
+                        }
+                        "configuration-version": {
+                            "data": {
+                                "type": "workspaces"
+                                "id": "''' + ConfigVersionID + '''"
+                            }
+                        }
+                    }
+                    }
+                }'''
+    serilaized = json.loads(payload)
+    # Creating Header content for POST request
+    headers_content = '{"Authorization" : "Bearer  ' + Token + '", "Content-Type" : "application/vnd.api+json"}'
+    headers = json.loads(headers_content)
+    url = "https://app.terraform.io/api/v2/runs"
     try:
+        # Creating a file to append the RUN information
         f = open("TFE_RUNID.txt", "a+")
-        buildkey = subprocess.Popen("env | grep 'bamboo_buildKey'", shell=True, stdout=subprocess.PIPE).stdout
-        buildnum = subprocess.Popen("env | grep 'bamboo_buildResultKey'", shell=True, stdout=subprocess.PIPE).stdout
-        buildkey = ((buildkey.read()).decode("utf-8")).split("=")
-        buildnum = ((buildnum.read()).decode("utf-8")).split("=")
-        comment = "Run Requested by Release for " + buildkey[1].replace("\n", "") + " build number " + buildnum[1].replace("\n", "")
-        print("\033[1;32m")
-        #payload = dict(
-         #   data=dict(attributes=dict(key=key, value=env_vars[key], category="terraform", hcl=False, sensitive=True),
-          #            relationships=dict(workspace=dict(data=dict(id=WorkSpaceID, type="workspaces")))), type="vars")
-        payload = dict(data=dict(attributes=dict(is-destroy=False, message=comment)), type="runs", relationships=dict(workspace=dict(data=dict(type="workspaces", id=WorkSpaceID)), configuration-version=dict(data=dict(type="configuration-versions", id=ConfigVersionID))))
+        # Initialize POST request
+        result = requests.post(url, json=serilaized, headers=headers, allow_redirects=False)
+        loaded_json = (json.loads(result.content))['data']
+        print(loaded_json)
+        if result.status_code in range(200, 203):
+            print("New Run created for workspace with WorkspaceID " + WorkSpaceID + "\n")
+            f.write("RunID: " + loaded_json['attributes']['id'])
     except Exception as e:
         print("Error: " + str(e) + "\n")
+    finally:
+        f.close()
+
 
 def main():
     workspaceid = sys.argv[1]
     configid = sys.argv[2]
-    workspacerun()
+    token = sys.argv[3]
+    workspacerun(workspaceid, configid, token)
+
+
 main()
