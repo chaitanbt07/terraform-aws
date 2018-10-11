34f63ca5bd31b3a407686c7c29f3813db90054f4
Raghavan R <raghavan.r@capgemini.com>
2018-10-11 14:47:12 +0530

    dump json data

--- release/scripts/Add-TerraformWorkspaceRun_step5.py
+++ release/scripts/Add-TerraformWorkspaceRun_step5.py
@@ -51,7 +51,7 @@ def workspacerun(WorkSpaceID, ConfigVersionID, Token):
     # Creating a file to append the RUN information
     f = open("TFE_RUNID.txt", "a+")
     # Initialize POST request
-    result = requests.post(url, json=serialized, headers=headers, allow_redirects=False)
+    result = requests.post(url, json=json.dumps(serialized), headers=json.dumps(headers), allow_redirects=False)
     print(result.content)
     #loaded_json = (json.loads(result.content))['data']
     #print(loaded_json)
