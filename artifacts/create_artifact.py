73f32117d788118dff668694717a2b7487bb150e
Raghavan R <33410658+raghavanr@users.noreply.github.com>
2018-10-15 15:10:30 +0530

    Update create_artifact.py

--- artifacts/create_artifact.py
+++ artifacts/create_artifact.py
@@ -2,12 +2,10 @@ import glob, tarfile
 tar = tarfile.open("terraform.tar.gz", "w:gz")
 for file in glob.glob("**/*.tf", recursive=True):
     try:
-        print("Creating Artifacts....")
-        print(file)
         tar.add(file)
     except Exception as e:
         print("Error :" + str(e))
 tar.close()
 t = tarfile.open("terraform.tar.gz", "r")
 for member in t.getmembers():
-    print(member.name)
+    print(member.name + " uploaded to artifactory")
