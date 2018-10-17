192294885f4eab8a95b1e88d7cb238269d9eeb13
Raghavan R <raghavan.r@capgemini.com>
2018-10-17 18:49:55 +0530

    adding exception handling

--- release/scripts/env_info.py
+++ release/scripts/env_info.py
@@ -3,57 +3,58 @@ import json
 import sys
 import botocore.exceptions
 import time
-dynamodb = boto3.client('dynamodb', region_name='ap-south-1', aws_access_key_id=sys.argv[1], aws_secret_access_key=sys.argv[2])
-def dynamodb_table_create():
-    try:
-
-        table = dynamodb.create_table(
-        TableName='terraform_data',
-        KeySchema=[
-            {
+try: 
+    dynamodb = boto3.client('dynamodb', region_name='ap-south-1', aws_access_key_id=sys.argv[1], aws_secret_access_key=sys.argv[2])
+    def dynamodb_table_create():
+        try:
+            table = dynamodb.create_table(
+            TableName='terraform_data',
+            KeySchema=[
+                {
                 'AttributeName': 'RunID',
                 'KeyType': 'HASH'
+                }
+            ],
+            AttributeDefinitions=[
+                {
+                    'AttributeName': 'RunID',
+                    'AttributeType': 'S'
+                }
+            ],
+            ProvisionedThroughput={
+                'ReadCapacityUnits': 5,
+                'WriteCapacityUnits': 5
             }
-        ],
-        AttributeDefinitions=[
-            {
-                'AttributeName': 'RunID',
-                'AttributeType': 'S'
-            }
-        ],
-        ProvisionedThroughput={
-            'ReadCapacityUnits': 5,
-            'WriteCapacityUnits': 5
-        }
-    )
-        print("TableARN: " + str(table['TableDescription']['TableArn']))
-        print("TableID: " + str(table['TableDescription']['TableId']))
-        print("TableRequestID: " + str(table['ResponseMetadata']['RequestId']))
-        print("TableCreationDate: " + str(table['TableDescription']['CreationDateTime']))
-        time.sleep(8)
-        dynamodb_put_data('RunID', sys.argv[3])
-    except dynamodb.exceptions.ResourceInUseException as e:
-        print("Table exists already,")
+        )
+            print("TableARN: " + str(table['TableDescription']['TableArn']))
+            print("TableID: " + str(table['TableDescription']['TableId']))
+            print("TableRequestID: " + str(table['ResponseMetadata']['RequestId']))
+            print("TableCreationDate: " + str(table['TableDescription']['CreationDateTime']))
+            time.sleep(8)
+            dynamodb_put_data('RunID', sys.argv[3])
+        except dynamodb.exceptions.ResourceInUseException as e:
+            print("Table exists already,")
+            try:
+                dynamodb_put_data('RunID', sys.argv[3])      
+            except Exception as ce:
+                print("Unable to load data into the table " + str(ce))
+        except botocore.exceptions.ClientError as ce:
+            print("Error Creating Table.." + str(ce.response['Error']['Message']))
+    def dynamodb_put_data(key, value):
         try:
-            dynamodb_put_data('RunID', sys.argv[3])      
-        except Exception as ce:
-            print("Unable to load data into the table " + str(ce))
+            dynamodb = boto3.resource('dynamodb', region_name='ap-south-1', aws_access_key_id=sys.argv[1], aws_secret_access_key=sys.argv[2])
+            update_table = dynamodb.Table('terraform_data')
+            json_data = {}
+            json_data[key] = value
+            response = update_table.put_item(Item=json_data)
+            if response['ResponseMetadata']['HTTPStatusCode'] == 200:
+                print("Data updated")
+            else:
+                print("Data updation failed!!")
+        except Exception as er:
+            print("Error uploading data: " + str(er))
+    
+    dynamodb_table_create() 
+except IndexError as ix:
+            print("Error: Check the given inputs " + str(ix))
 
-def dynamodb_put_data(key, value):
-    try:
-        dynamodb = boto3.resource('dynamodb', region_name='ap-south-1', aws_access_key_id=sys.argv[1], aws_secret_access_key=sys.argv[2])
-        update_table = dynamodb.Table('terraform_data')
-        json_data = {}
-        json_data[key] = value
-        response = update_table.put_item(Item=json_data)
-        if response['ResponseMetadata']['HTTPStatusCode'] == 200:
-            print("Data updated")
-        else:
-            print("Data updation failed!!")
-    except Exception as er:
-        print("Error uploading data: " + str(er))
-
-def main():
-    dynamodb_table_create()
-
-main()
