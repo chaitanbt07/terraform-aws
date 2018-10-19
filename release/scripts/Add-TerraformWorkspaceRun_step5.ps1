c2541b3acb90167685e2e6793893a0a501fcd550
Raghavan R <33410658+raghavanr@users.noreply.github.com>
2018-10-19 11:56:46 +0530

    Update Add-TerraformWorkspaceRun_step5.ps1

--- release/scripts/Add-TerraformWorkspaceRun_step5.ps1
+++ release/scripts/Add-TerraformWorkspaceRun_step5.ps1
@@ -37,7 +37,7 @@
 
         Write-Host "$($MyInvocation.MyCommand.Name): Script execution started"
 
-        $Comment = "Run Requested By Release $($env:deploy.release) for $($env:buildResultKey) build number $($env:buildNumber) "
+        $Comment = "Run Requested By Release $($env:bamboo_deploy.release) for $($env:bamboo_buildKey) build number $($env:bamboo_buildResultKey) "
         
         $Json = @{
           "data"= @{
