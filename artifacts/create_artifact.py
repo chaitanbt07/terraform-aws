4ad6dbbed2e23931a8c87a3697a6bd64f79a3123
Raghavan R <raghavan.r@capgemini.com>
2018-11-01 10:47:44 +0530

    changing artifact creation files for sentinel scripts

--- artifacts/create_artifact.py
+++ artifacts/create_artifact.py
@@ -1,11 +1,19 @@
-import glob, tarfile
-tar = tarfile.open("terraform.tar.gz", "w:gz")
-for file in glob.glob("**/*.tf", recursive=True):
-    try:
-        tar.add(file)
-    except Exception as e:
-        print("Error :" + str(e))
-tar.close()
-t = tarfile.open("terraform.tar.gz", "r")
-for member in t.getmembers():
-    print(member.name + " uploaded to artifactory")
+import glob, tarfile, sys
+
+def create_artifact(filename, searchstring):
+    tar = tarfile.open(filename, "w:gz")
+    for file in glob.glob("**/*." + searchstring, recursive=True):
+        try:
+            tar.add(file)
+        except Exception as e:
+            print("Error :" + str(e))
+    tar.close()
+    t = tarfile.open(filename, "r")
+    for member in t.getmembers():
+        print(member.name + " uploaded to artifactory")
+
+def main():
+    filename = sys.argv[1]
+    searchstring = sys.argv[2]
+    create_artifact(filename, searchstring)
+main()
